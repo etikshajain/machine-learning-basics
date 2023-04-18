143a7ee7c560626800d689bcffdae007030206df
etikshajain <jainetiksha@gmail.com>
2023-04-18 23:16:59 +0530

    linear regr sklearn model

--- regression/price_prediction.py
+++ regression/price_prediction.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -21,4 +25,21 @@ forecast_out = int(math.ceil(0.01*len(df))) # we want to predict the price of 0.
 df['label'] = df[forecast_col].shift(-forecast_out) # make the label column as the price of 0.1*len(df) days later
 df.dropna(inplace=True)
 
-print(df.tail())
+# defining features and labels
+X = np.array(df.drop(['label'],1))
+Y = np.array(df['label'])
+
+# preprocessing the data - scaling the features between -1 and 1
+X = preprocessing.scale(X)
+
+# creating training and test data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# classifier
+clf = LinearRegression()
+# train the data
+clf.fit(X_train, Y_train)
+# get test score
+accuracy = clf.score(X_test, Y_test)
+
+print(accuracy)
