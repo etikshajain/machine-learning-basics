bfbf01cf9ddd0eb970bd26310867dcac05d2e915
etikshajain <jainetiksha@gmail.com>
2023-04-18 23:45:41 +0530

    predict data and plot

--- regression/price_prediction.py
+++ regression/price_prediction.py
@@ -2,9 +2,14 @@ import pandas as pd
 import quandl
 import math
 import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -23,14 +28,18 @@ df.fillna(-99999, inplace=True)
 forecast_col = 'Adj. Close'
 forecast_out = int(math.ceil(0.01*len(df))) # we want to predict the price of 0.1*len(df) days later
 df['label'] = df[forecast_col].shift(-forecast_out) # make the label column as the price of 0.1*len(df) days later
-df.dropna(inplace=True)
+# df.dropna(inplace=True)
 
 # defining features and labels
 X = np.array(df.drop(['label'],1))
-Y = np.array(df['label'])
 
 # preprocessing the data - scaling the features between -1 and 1
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+df.dropna(inplace=True)
+
+Y = np.array(df['label'])
 
 # creating training and test data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
@@ -43,3 +52,26 @@ clf.fit(X_train, Y_train)
 accuracy = clf.score(X_test, Y_test)
 
 print(accuracy)
+
+# predict outputs
+forecast_set = clf.predict(X_lately)
+print(forecast_set, forecast_out)
+
+# graph
+df['Forecast'] = np.nan #unpredicted data
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix+=one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
